2781828f5c3fd6ab24fa4ab441304abeb090609e
AzharBadri05 <azharbadri02@gmail.com>
2026-03-05 16:34:27 +0700

    barcode ttd pdf all form

--- resources/views/qc-sistem/pemeriksaan-barang-mudah-pecah/pdf-report.blade.php
+++ resources/views/qc-sistem/pemeriksaan-barang-mudah-pecah/pdf-report.blade.php
@@ -208,7 +208,21 @@
         }
 
         .signature-space {
+            height: 60px;
+            margin: 0 10px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .signature-line-empty {
             height: 45px;
+            width: 100%;
+        }
+        
+        .qr-code-img {
+            max-height: 55px;
+            max-width: 55px;
         }
 
         .signature-name {
@@ -348,17 +362,50 @@
             <tr>
                 <td class="signature-cell">
                     <div class="signature-header-item">Dibuat Oleh (QC)</div>
-                    <div class="signature-space"></div>
+                    <div class="signature-space">
+                        @if($qcName)
+                            @php
+                                $qcQrData = "Dokumen ini telah diverifikasi secara sistem oleh {$qcName} (Tim QC)";
+                                $qcQrCodeSvg = \SimpleSoftwareIO\QrCode\Facades\QrCode::size(55)->generate($qcQrData);
+                                $base64QcSvg = "data:image/svg+xml;base64," . base64_encode($qcQrCodeSvg);
+                            @endphp
+                            <img src="{{ $base64QcSvg }}" class="qr-code-img" alt="QR Code QC">
+                        @else
+                            <div class="signature-line-empty"></div>
+                        @endif
+                    </div>
                     <div class="signature-name">{{ $qcName ?: '-' }}</div>
                 </td>
                 <td class="signature-cell">
                     <div class="signature-header-item">Disetujui Oleh (Tim Warehouse)</div>
-                    <div class="signature-space"></div>
+                    <div class="signature-space">
+                        @if($produksiName)
+                            @php
+                                $prodQrData = "Dokumen ini telah diverifikasi secara sistem oleh {$produksiName} (Tim Warehouse)";
+                                $prodQrCodeSvg = \SimpleSoftwareIO\QrCode\Facades\QrCode::size(55)->generate($prodQrData);
+                                $base64ProdSvg = "data:image/svg+xml;base64," . base64_encode($prodQrCodeSvg);
+                            @endphp
+                            <img src="{{ $base64ProdSvg }}" class="qr-code-img" alt="QR Code Warehouse">
+                        @else
+                            <div class="signature-line-empty"></div>
+                        @endif
+                    </div>
                     <div class="signature-name">{{ $produksiName ?: '-' }}</div>
                 </td>
                 <td class="signature-cell">
                     <div class="signature-header-item">Diverifikasi Oleh (SPV QC)</div>
-                    <div class="signature-space"></div>
+                    <div class="signature-space">
+                        @if($spvName)
+                            @php
+                                $spvQrData = "Dokumen ini telah diverifikasi secara sistem oleh {$spvName} (Tim Supervisor QC)";
+                                $spvQrCodeSvg = \SimpleSoftwareIO\QrCode\Facades\QrCode::size(55)->generate($spvQrData);
+                                $base64SpvSvg = "data:image/svg+xml;base64," . base64_encode($spvQrCodeSvg);
+                            @endphp
+                            <img src="{{ $base64SpvSvg }}" class="qr-code-img" alt="QR Code SPV">
+                        @else
+                            <div class="signature-line-empty"></div>
+                        @endif
+                    </div>
                     <div class="signature-name">{{ $spvName ?: '-' }}</div>
                 </td>
             </tr>
